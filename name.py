5e4575e60c2aa74a060e42c2e26d57d380843382
jchandd <jaydenc243@deltalearns.ca>
2026-02-10 10:24:29 -0800

    added second task

--- name.py
+++ name.py
@@ -26,4 +26,47 @@ def get_encouragement() -> str:
 print(get_encouragement())
 
 
-# TASK
+# TASK: Write fuction to ask for valid age input
+
+def get_valid_age(prompt: str) -> int:
+    """Asks the user for valid age input, ensure positive integer"""
+    while True:
+        try:
+            age = int(input(prompt))
+            if age > 0:
+                return age
+            else:
+                print("Enter positive number")
+            except ValueError:
+                print("Invalid input. Please enter number")
+
+# Call function and store results
+user_age = get_valid_age("Enter age")
+
+# Display valid age
+print(f"Your valid age: {user_age}")
+
+
+# TASK write function to reverse words and capitalize
+
+def reverse_and_capitalize(word: str) -> str:
+    """
+    Reverse word and capitalize
+
+    :param word: Input word
+    :return: reversed word uppercased
+    """
+
+    # Reverse the word
+    reversed_word = word[::-1]
+
+    return reversed_word.upper()
+
+# Prompt user to enter word
+user_word = input("Enter word: ")
+
+# Call function to store result
+reversed_result = reverse_and_capitalize(user_word)
+
+# Display new word
+print(f"Reversed and capitalized: {reversed_result}")
